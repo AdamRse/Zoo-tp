8a6bd0d98af351c6d25b7b370a84f3c93a675ba2
AdamRse <bakaarion3@gmail.com>
2024-02-13 16:58:51 +0100

    ajout map et positions

--- classes/Zoo.class.php
+++ classes/Zoo.class.php
@@ -4,25 +4,75 @@ class Zoo {
     private const enclosMax = 10;
 
     protected $_enclosPosition = array(
-        [0, 0]
-        ,[10, 20]
-        ,[20, 70]
-        ,[30, 20]
-        ,[40, 60]
-        ,[50, 40]
-        ,[60, 80]
-        ,[70, 40]
-        ,[80, 10]
-        ,[90, 60]
+        [5, 35]
+        ,[10, 31]
+        ,[8, 23]
+        ,[26, 21]
+        ,[28, 37]
+        ,[42, 47]
+        ,[17, 43]
+        ,[52, 84]
+        ,[51, 20]
+        ,[42, 14]
+        ,[64, 35]
+        ,[84, 32]
+        ,[92, 44]
+        ,[27, 67]
+    );
+    protected $_volierePosition = array(
+        [51.5, 9]
+        ,[59, 9]
+        ,[41, 37]
+        ,[49, 43]
+        ,[53, 55]
+        ,[57.5, 29]
+        ,[75, 38]
+        ,[83, 48]
+        ,[71, 64]
+        ,[53, 71]
+        ,[7, 52]
+        ,[32, 59]
+        ,[17, 10]
+    );
+    protected $_aquaPosition = array(
+        [28, 49]
+        ,[19, 56]
+        ,[20, 36]
+        ,[37, 24]
+        ,[68, 25]
+        ,[57, 42]
+        ,[62, 53]
+        ,[46, 61]
+        ,[44, 76]
     );
     public function afficherMap(){
         ?>
-        <div class="relative inline-block w-screen overflow-hidden">
+        <div class="relative inline-block w-full overflow-hidden select-none">
+            <div class="absolute pt-5 pl-7">
+                <i id="btMenu" class="p-2 cursor-pointer fa-solid fa-bars text-<?= COLOR_THEME_TW ?>-700 fa-2x"></i>
+                <i id="btStats" class="p-2 cursor-pointer fa-regular fa-compass text-<?= COLOR_THEME_TW ?>-700 fa-2x"></i>
+            </div>
             <img src="/images/map.png" class="w-full top-0 left-0"/>
             <?php
             foreach($this->_enclosPosition as $xy){
                 ?>
-                <div class="enclos bg-black inline-block absolute rotate-45" style="left: <?= $xy[0] ?>%; top: <?= $xy[1] ?>%"></div>
+                <div class="enclos inline-block absolute rounded-full shadow-2xl border cursor-pointer" style="left: <?= $xy[0] ?>%; top: <?= $xy[1] ?>%; background: center/150% url('./images/enclos.png')">
+                <div class="px-2 rounded-md text-center text-<?= COLOR_THEME_TW ?>-700 bg-<?= COLOR_THEME_TW ?>-200 mt-14">Enclos</div>
+                </div>
+                <?php
+            }
+            foreach($this->_volierePosition as $xy){
+                ?>
+                <div class="enclos inline-block absolute rounded-full shadow-2xl border cursor-pointer" style="left: <?= $xy[0] ?>%; top: <?= $xy[1] ?>%; background: center/150% url('./images/voliere.png')">
+                <div class="px-2 min-w-16 rounded-md text-center text-<?= COLOR_THEME_TW ?>-700 bg-<?= COLOR_THEME_TW ?>-200 mt-14">Volière</div>
+                </div>
+                <?php
+            }
+            foreach($this->_aquaPosition as $xy){
+                ?>
+                <div class="text-center enclos inline-block absolute rounded-full shadow-2xl border cursor-pointer" style="left: <?= $xy[0] ?>%; top: <?= $xy[1] ?>%; background: center/150% url('./images/aquarium.png')">
+                    <div class="px-2 min-w-24 rounded-md text-center text-<?= COLOR_THEME_TW ?>-700 bg-<?= COLOR_THEME_TW ?>-200 mt-14">Aquarium</div>
+                </div>
                 <?php
             }
 
