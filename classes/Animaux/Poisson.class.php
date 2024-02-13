a2c40c0059964845e89eac601a2ef9c02ce6e700
RomainRon <rondepierreromain@gmail.com>
2024-02-13 10:31:22 +0100

    Animaux construct

--- classes/Animaux/Poisson.class.php
+++ classes/Animaux/Poisson.class.php
@@ -2,6 +2,12 @@
 namespace Animaux;
 class Poisson extends Animal
 {
-
+    public function __construct($id, $poids = 0.0002, $taille = 0.01)
+    {
+        parent::__construct($id, $poids, $taille);
+        $this->setName("Poisson");
+        $this->setCrie("rote");
+    }
 
 }
+
